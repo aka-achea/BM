7fd199fdde114f3ff407f90cfbbc0074b239e5c6
aka-achea <cjyrb@hotmail.com>
2020-07-28 23:00:45 +0800

    'update'

--- core/imap.py
+++ core/imap.py
@@ -12,21 +12,24 @@ from mylog import get_funcname, mylogger
 from config import mailsvr,user,key,logfile
 
 
-try:
-    print(mailsvr)
-    M = imaplib.IMAP4_SSL('imap.163.com')
-    M.login(user,key)
-except TimeoutError as e:
-    ml.err(e)
-    ml.err('Retry')
+from imapclient import IMAPClient
 
-M.select()
-typ, data = M.search(None, 'ALL')
-for num in data[0].split():
-    typ, data = M.fetch(num, '(RFC822)')
-    print('Message %s\n%s\n' % (num, data[0][1]))
-M.close()
-M.logout()
+# context manager ensures the session is cleaned up
+with IMAPClient(host="imap.163.com") as client:
+    client.login(user,key)
+    client.select_folder('INBOX')
 
+    # search criteria are passed in a straightforward way
+    # (nesting is supported)
+    messages = client.search(['NOT', 'DELETED'])
 
+    # fetch selectors are passed as a simple list of strings.
+    response = client.fetch(messages, ['FLAGS', 'RFC822.SIZE'])
 
+    # `response` is keyed by message id and contains parsed,
+    # converted response items.
+    for message_id, data in response.items():
+        print('{id}: {size} bytes, flags={flags}'.format(
+            id=message_id,
+            size=data[b'RFC822.SIZE'],
+            flags=data[b'FLAGS']))
